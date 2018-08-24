d5d49ddfae2cbeb189a8cf6603fa07cc28432cc6
tonglei <tonglei@qq.com>
2018-08-24 15:27:13 +0800

    update the sweetest command

--- sweetest/sweetest/__init__.py
+++ sweetest/sweetest/__init__.py
@@ -16,4 +16,5 @@ def sweetest():
     current_dir = os.getcwd()
     extract(os.path.join(example_dir, 'sweetest_example.zip'), current_dir)
 
-    print('生成 sweetest example 成功')
+    print('\n生成 sweetest example 成功\n\n详细使用方法请关注公众号：Sweetest自动化测试\nQQ交流群：158755338 (验证码：python)')
+    print('\n\n快速体验，请输入如下命令，进入示例目录，启动运行脚本\n\ncd sweetest_example\npython start.py')
