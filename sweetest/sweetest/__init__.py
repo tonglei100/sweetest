bbf2bace9a5278942fe3234b25a40cc9449ffeb1
tonglei <tonglei@qq.com>
2019-06-21 13:37:38 +0800

    update tips

--- sweetest/sweetest/__init__.py
+++ sweetest/sweetest/__init__.py
@@ -17,5 +17,5 @@ def sweetest():
     example_dir = sweetest_dir / 'example' / 'sweetest_example.zip'
     extract(str(example_dir), Path.cwd())
 
-    print('\n文档: https://doc.sweeter.io\n社区: https://sweeter.io\n\n公众号：喜文测试\nQ Q 群：158755338 (验证码：python)注意首字母小写')
-    print('\n\n生成 sweetest example 成功\n快速体验，请输入如下命令，进入示例目录，启动运行脚本\n\ncd sweetest_example\npython start.py')
+    print('\n文档: https://sweeter.io\n公众号：喜文测试\nQ Q 群：158755338 (验证码：python)注意首字母小写')
+    print('\n\n生成 sweetest example 成功\n快速体验，请输入如下命令(进入示例目录，启动运行脚本):\n\ncd sweetest_example\npython start.py')
